7df3ff4c06faa38e4637cbb8f433de7b2c3f8834
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-07-03 16:25:18 -0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiUrl: "https://pingobras-factorio-server.glitch.me/api",
+  apiUrl: "https://pingobras-factorio-server.onrender.com/api",
   ipToken: "f1dd48b5ecd676",
   encodedUser: "=",
   encodedPassword: "==",
