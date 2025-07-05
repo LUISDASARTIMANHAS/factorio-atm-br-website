913cc44378f6cc6f85143a049e8d566f397f5f60
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-07-05 18:40:43 -0300

    FIXED loader

--- src/js/load-scripts.js
+++ src/js/load-scripts.js
@@ -1,20 +1,32 @@
 (() => {
   const autoscripts = document.querySelector("autoscripts");
   const fonteUser = "/factorio-atm-br-website/src/js/";
-  const src = [
+  const srcModule = [
     "manutencao-redirect",
     "offline",
     "message",
     "ip-info",
+  ];
+  const srcCJS = [
     "functions",
   ];
 
-  for (let i = 0; i < src.length; i++) {
+  for (let i = 0; i < srcModule.length; i++) {
     var newScript = document.createElement("script");
-    var url = fonteUser + src[i] + ".js";
+    var url = fonteUser + srcModule[i] + ".js";
 
     newScript.setAttribute("src",url );
     newScript.setAttribute("type", "module"); // ESSENCIAL para usar export/import
+    autoscripts.appendChild(newScript);
+
+    console.log(`%c [SISTEMA ATM BR]: Novo script ESM: ${url}`,"#ffaa00")
+  }
+
+  for (let i = 0; i < srcCJS.length; i++) {
+    var newScript = document.createElement("script");
+    var url = fonteUser + srcCJS[i] + ".js";
+
+    newScript.setAttribute("src",url );
     autoscripts.appendChild(newScript);
 
     console.log(`%c [SISTEMA ATM BR]: Novo script: ${url}`,"#ffaa00")
