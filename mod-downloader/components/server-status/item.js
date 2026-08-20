8a4bc28bab4786a63d2205ba31e460f1b9b139f5
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-08-19 23:13:59 -0300

    updated dependencies colors

--- mod-downloader/components/server-status/item.js
+++ mod-downloader/components/server-status/item.js
@@ -9,6 +9,7 @@ export function createStatusItem(label, dataKey) {
 	const span = document.createElement("span");
 
 	span.innerHTML = `
+				<!-- StatusItem -->
 				<strong>${label}:</strong>
 				<span data-${dataKey}>-</span>
 		`;
